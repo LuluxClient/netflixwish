6f1d20519d0ab98a92d0727b97ec895f4aa49d38
Luca Améaume <62576432+LuluxClient@users.noreply.github.com>
2023-10-30 14:37:14 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Navbar from "./components/Navbar";
 import SearchMovie from "./pages/SearchMovie";
 import Trending from "./pages/Trending";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Trending />} />
           <Route path="/search_movie" element={<SearchMovie />} />
